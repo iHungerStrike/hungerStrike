bd6ec8ac46547be924f6a3cec517babbbfa10259
Leonvdk <leonvandeklundert@gmail.com>
2020-11-20 10:20:33 +0000

    Styling is done and functional. Next step: link icons through CurrentWeather

--- src/components/Weather/WeekForecast.js
+++ src/components/Weather/WeekForecast.js
@@ -3,20 +3,20 @@ import './forecast.css'
 
 const WeekForecast = (props) => {
   console.log(props)
+
   return(
     <div className="day-card">
       <div>icon</div>
       <div classNmae="temp">
-      <b>&#x2600; {props.temp.day}</b>
-      <p>&#x263E; {props.temp.eve}</p>
+      <b className="solid-text">&#x2600; {props.temp.day}&deg;</b><br />
+      <span className="solid-text">  &#x263E; {props.temp.eve}&deg;</span>
       </div>
-      <p>&#9872; {props.wind_speed} km/h</p>
+      <span className="solid-text">&#x2691; {props.wind_speed} <sup>km/h</sup></span>
       {/* <em>{props.weather[0].description}</em> */}
       {props.rain&& (
         <div className="rain">
-        <p>&#x2602; {props.rain}mm</p>
+        <span className="solid-text">&#x2602; {props.rain}<sup>mm</sup></span>
         </div>
-
       )}
     </div>
   )
